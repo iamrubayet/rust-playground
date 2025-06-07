e911e37f5d31d42d7051aab40936e031d1e5498a
Shahriar Rahman Rubayet <shahriar.rubayet@technonext.com>
2025-06-08 01:50:10 +0600

    chore: Added

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ fn main() {
     println!("Hello, world!");
 }
 
-// variable
-fn main_second() {
-    let x = 5;
-    let y: u32 = 10;
-    let z : f32= 3.14;
-    println!("The value of x is: {}", x);
+// // variable
+// fn main_second() {
+//     let x = 5;
+//     let y: u32 = 10;
+//     let z : f32= 3.14;
+//     println!("The value of x is: {}", x);
 
-    let mut r : i8  = 20; // overflow
+//     let mut r : i8  = 20; // overflow
 
-    for i in 0..10000 {
-        r = r+100;
-    }
+//     for i in 0..10000 {
+//         r = r+100;
+//     }
 
-}
+// }
 
