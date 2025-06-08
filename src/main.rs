3ea7dcc5afdea0cb440191744837494fd7ba26ac
Shahriar Rahman Rubayet <shahriar.rubayet@technonext.com>
2025-06-08 18:45:36 +0600

    chore: Mutalibilty

--- src/main.rs
+++ src/main.rs
@@ -35,16 +35,32 @@
 
 // fibonacci recurion
 
-fn main() {
-    let n = 10;
-    println!("Fibonacci of {} is {}", n, fibonacci(n));
-}
-fn fibonacci(n: u32) -> u32 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
+// fn main() {
+//     let n = 10;
+//     println!("Fibonacci of {} is {}", n, fibonacci(n));
+// }
+// fn fibonacci(n: u32) -> u32 {
+//     if n == 0 {
+//         return 0;
+//     } else if n == 1 {
+//         return 1;
+//     } else {
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+//     }
+// }
+
+// fn fib(n: u32) -> u32 {
+//     if n == 0 {
+//         return 0;
+//     }
+//     let mut a = 0;
+//     let mut b = 1;
+//     for _ in 2..=n {
+//         let temp = b;
+//         b = a + b;
+//         a = temp;
+//     }
+//     b
+// }
+
+
