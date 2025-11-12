97d0b0baa9cd3dc01bc015b95099532a261377d9
Shahriar Rahman Rubayet <shahriar.rubayet@technonext.com>
2025-11-12 13:46:11 +0600

    chore: Added data types code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 
-
+// std library example
 
 // fn main() {
 //     println!("what is your name?");
@@ -20,14 +20,31 @@ use std::cmp::Ordering;
 //     println!("Hello, world!");
 // }
 
+// shadowing example
+// fn main() {
+//     const ONE_MILLION: u32 = 1_000_000;
+//     println!("one million is written as {}", ONE_MILLION);
 
-fn main() {
-    const ONE_MILLION: u32 = 1_000_000;
-    println!("one million is written as {}", ONE_MILLION);
+//     let age = "47";
+//     let mut age: u32 = age.trim().parse().expect("age wasn't assigned a number");
+//     age += 1;
+//     println!("next year, you will be {}", age);
+// }
 
-    let age = "47";
-    let mut age: u32 = age.trim().parse().expect("age wasn't assigned a number");
-    age += 1;
-    println!("next year, you will be {}", age);
+// data types example
+
+// fn main(){
+//     let is_true : bool = true;
+//     let a_float : f64 = 1.0; // regular annotation
+//     let an_integer = 5i32; // suffix annotation
+//     let default_float = 3.0; // f64
+//     let default_integer = 7; // i32
+//     let mut mutable = 12; // mutable i32
+//     mutable = 21;
+
+// }
+
+// math
+fn main(){
+    
 }
-
