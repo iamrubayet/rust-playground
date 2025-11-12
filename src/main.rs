48849359460deaf8ae9a67af58d79119d7ea7629
Shahriar Rahman Rubayet <shahriar.rubayet@technonext.com>
2025-11-12 13:34:32 +0600

    chore: Added shadowing

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,27 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 
-fn main() {
-    println!("what is your name?");
-    let mut name = String::new();
-    println!("Hello, world!");
-}
 
+
+// fn main() {
+//     println!("what is your name?");
+//     let mut name = String::new();
+//     let greet = "nice to meet you";
+//     io::stdin().read_line(&mut name).expect("failed to read line");
+
+//     println!("Hello, {}! {}", name.trim_end(), greet);
+
+//     println!("Hello, world!");
+// }
+
+
+fn main() {
+    const ONE_MILLION: u32 = 1_000_000;
+    println!("one million is written as {}", ONE_MILLION);
+
+    let age = "47";
+    let mut age: u32 = age.trim().parse().expect("age wasn't assigned a number");
+    age += 1;
+    println!("next year, you will be {}", age);
+}
 
