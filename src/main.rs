81d526af3ff8442cd9d2cc77d2694a75849efdcc
Shahriar Rahman Rubayet <shahriar.rubayet@technonext.com>
2025-11-12 19:11:30 +0600

    chore: Loop

--- src/main.rs
+++ src/main.rs
@@ -45,17 +45,115 @@ use std::cmp::Ordering;
 // }
 
 // math
+// fn main(){
+//     let num_5: u32 = 5;
+//     let num_4: u32 = 4;
+//     let sum = num_5 + num_4;
+//     let difference = num_5 - num_4;
+//     let product = num_5 * num_4;
+//     let quotient = num_5 / num_4;
+//     let remainder = num_5 % num_4;
+//     println!("sum: {}", sum);
+//     println!("difference: {}", difference);
+//     println!("product: {}", product);
+//     println!("quotient: {}", quotient);
+//     println!("remainder: {}", remainder);
+// }
+
+// random number generator example
+// fn main(){
+//     let secret_number = rand::thread_rng().gen_range(1..=100);
+//     println!("The secret number is: {}", secret_number);
+// }
+
+// if else 
+// fn main()
+// {
+//     let age = 8;
+
+//     if (age >= 1) && (age <= 18){
+//         println!("you get a child discount");
+//     }
+//     else if (age >= 19) && (age <= 65){
+//         println!("you pay full price");
+//     }
+//     else{
+//         println!("you get a senior citizen discount");
+//     }
+
+//     let age = 8;
+
+//     let can_vote = if age >= 18 { true } else { false };
+
+//     let age2 = 20;
+
+//     match age2 {
+//         1..=18 => println!("you get a child discount"),
+//         19..=65 => println!("you pay full price"),
+//         _ => println!("you get a senior citizen discount"),
+//     }
+
+
+//     let my_age = 47;
+
+//     let voting_age = 18;
+
+//     match my_age.cmp(&voting_age) {
+//         Ordering::Less => println!("you are not old enough to vote"),
+//         Ordering::Greater => println!("you are old enough to vote"),
+//         Ordering::Equal => println!("you are exactly old enough to vote"),
+//     }
+
+// }
+
+
+// array 
+
 fn main(){
-    let num_5: u32 = 5;
-    let num_4: u32 = 4;
-    let sum = num_5 + num_4;
-    let difference = num_5 - num_4;
-    let product = num_5 * num_4;
-    let quotient = num_5 / num_4;
-    let remainder = num_5 % num_4;
-    println!("sum: {}", sum);
-    println!("difference: {}", difference);
-    println!("product: {}", product);
-    println!("quotient: {}", quotient);
-    println!("remainder: {}", remainder);
+    let array1 = [1,2,3,4,5];
+    let array2: [i32;5] = [1,2,3,4,5];
+    let array3 = [3;5]; // all elements are 3 and length is 5
+
+    println!("first element of array1: {}", array1[0]);
+    println!("second element of array2: {}", array2[1]);
+    println!("third element of array3: {}", array3[2]);
+
+    // length of array
+    println!("length of array1: {}", array1.len());
+
+    let mut loop_index = 0; 
+
+    // looping through array using while
+    loop {
+        if array1[loop_index] % 2 == 0 {
+            println!("{} is even", array1[loop_index]);
+        } else {
+            println!("{} is odd", array1[loop_index]);
+        }
+        loop_index += 1;
+        if loop_index >= array1.len() {
+            break;
+        }
+    }
+
+
+    while loop_index < array2.len() {
+        if array2[loop_index] % 2 == 0 {
+            println!("{} is even", array2[loop_index]);
+        } else {
+            println!("{} is odd", array2[loop_index]);
+        }
+        loop_index += 1;
+    }
+
+
+
+    for val in array3.iter() {
+        if *val % 2 == 0 {
+            println!("{} is even", *val);
+        } else {
+            println!("{} is odd", *val);
+        }
+    }
+
 }
